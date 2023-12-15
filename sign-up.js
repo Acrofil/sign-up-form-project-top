7aabb8f2bd529dbd4c782b4286c20da51657f89c
Acrofil <axpeshev@gmail.com>
2023-12-15 16:18:08 +0200

    Handle submit behavior

--- sign-up.js
+++ sign-up.js
@@ -1,4 +1,5 @@
 const submitBtn = document.querySelector(".btn");
+const form = document.querySelector("form");
 
 let pass1 = document.querySelector(".pass1");
 let pass2 = document.querySelector(".pass2");
@@ -6,24 +7,36 @@ let pass2 = document.querySelector(".pass2");
 let cssStyleWrong = "border: 3px solid red;";
 let cssStyleMatch = "border: 3px solid #05d9e8";
 
+let passwordsMatch = false;
+
 pass2.onkeyup = function() {
     
     if (pass1.value !== pass2.value) {
-        pass2.setCustomValidity("Password not matching!");
+        pass2.setCustomValidity("Passwords not matching!");
         pass2.reportValidity();
         pass2.setCustomValidity("");
         pass1.style.cssText = cssStyleWrong;
         pass2.style.cssText = cssStyleWrong;
-        submitBtn.setAttribute("disabled", "");
+        passwordsMatch = false;
 
     } else if ([pass2.value === pass1.value]) {
-        pass2.setCustomValidity("Password match!");
+        pass2.setCustomValidity("Passwords match!");
         pass2.reportValidity();
         pass2.setCustomValidity("");
         pass1.style.cssText = cssStyleMatch;
         pass2.style.cssText = cssStyleMatch;
-        submitBtn.removeAttribute("disabled", "");
-
+        passwordsMatch = true;
     };
-
 };
+
+submitBtn.addEventListener("click", (e) => {
+
+    if (!passwordsMatch) {
+        submitBtn.setCustomValidity("Check passwords!");
+        submitBtn.reportValidity();
+        e.preventDefault();
+        
+    } else {
+        form.submit();
+    }
+});
